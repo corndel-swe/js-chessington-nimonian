a1dcc2b81413c676bd3a98093d5e064a5e9ee3c7
Simon Haines <nimon@hotmail.co.uk>
2024-06-06 09:21:10 +0100

    solves d2e2

--- exercises/maths.js
+++ exercises/maths.js
@@ -1,1 +1,11 @@
-export class Maths {}
+export class Maths {
+  static PI = 3.14
+
+  static max(a, b) {
+    return (a + b + Maths.abs(b - a)) / 2
+  }
+
+  static abs(a) {
+    return a > 0 ? a : -a
+  }
+}
