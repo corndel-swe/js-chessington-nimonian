784f2a5e394c0e8a6013a9079e4c158bb65a8a3a
Simon Haines <nimon@hotmail.co.uk>
2024-06-06 09:00:32 +0100

    adds knight moves

--- src/models/pieces/knight.js
+++ src/models/pieces/knight.js
@@ -1,3 +1,4 @@
+import Square from '../square.js'
 import Piece from './piece.js'
 
 export default class Knight {
@@ -6,7 +7,28 @@ export default class Knight {
   }
 
   getAvailableMoves(board) {
-    return []
+    const loc = board.findPiece(this)
+    const moves = []
+
+    const dirs = [
+      { dr: 2, dc: 1 },
+      { dr: 2, dc: -1 },
+      { dr: -2, dc: 1 },
+      { dr: -2, dc: -1 },
+      { dr: 1, dc: 2 },
+      { dr: -1, dc: 2 },
+      { dr: 1, dc: -2 },
+      { dr: -1, dc: -2 }
+    ]
+
+    for (let { dr, dc } of dirs) {
+      const candidate = new Square(loc.row + dr, loc.col + dc)
+      if (board.contains(candidate)) {
+        moves.push(candidate)
+      }
+    }
+
+    return moves
   }
 
   moveTo(board, newSquare) {
