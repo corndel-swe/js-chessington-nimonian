3c8c115246d9206e96479c96a98b3673ebe6422e
Simon Haines <nimon@hotmail.co.uk>
2024-06-06 08:43:28 +0100

    adds bishop moves

--- src/models/pieces/bishop.js
+++ src/models/pieces/bishop.js
@@ -8,8 +8,6 @@ export default class Bishop {
 
   getAvailableMoves(board) {
     const loc = board.findPiece(this)
-    console.log(loc)
-
     const moves = []
     const dirs = [
       { dr: 1, dc: 1 },
@@ -25,8 +23,6 @@ export default class Bishop {
         candidate = new Square(candidate.row + dir.dr, candidate.col + dir.dc)
       }
     }
-
-    console.table(moves)
 
     return moves
   }
